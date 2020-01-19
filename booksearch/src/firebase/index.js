9a0b71e3a7fbf2cc8099d8ae2ad16165e538c005
hebishima shogo <hebishima.team@gmail.com>
2020-01-19 21:28:33 +0900

    プロと追加

---
+++ booksearch/src/firebase/index.js
@@ -0,0 +1,7 @@
+// import firebase from 'firebase';
+import firebase from "firebase/app"
+import "firebase/firestore"
+import { firebaseConfig } from './config';
+
+firebase.initializeApp(firebaseConfig);
+export const firebaseDb = firebase.firestore();
