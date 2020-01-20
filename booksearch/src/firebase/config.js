88d96531079ad0a48317ab64ffdcc876f5abc2d7
hebishima shogo <hebishima.team@gmail.com>
2020-01-20 21:57:38 +0900

    機密情報削除

--- booksearch/src/firebase/config.js
+++ booksearch/src/firebase/config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCTxPujY5WP-ZNHYyXT4MuX-tmvCWP_gaE",
-  authDomain: "shosekiappproject.firebaseapp.com",
-  databaseURL: "https://shosekiappproject.firebaseio.com",
-  projectId: "shosekiappproject",
-  storageBucket: "shosekiappproject.appspot.com",
-  messagingSenderId: "596910785917",
-  appId: "1:596910785917:web:2b28fca01a89957a95b814",
-  measurementId: "G-H7ZPPQ3VB3"
+  apiKey: "*****",
+  authDomain: "*****",
+  databaseURL: "****",
+  projectId: "****",
+  storageBucket: "****",
+  messagingSenderId: "****",
+  appId: "****",
+  measurementId: "****"
 };
