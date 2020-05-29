ac5bb741a8f6f596da9f12fb2ac47908f1669d37
hebishima shogo <hebishima.team@gmail.com>
2020-05-29 20:09:51 +0900

    デプロイできないバグを修正

--- booksearch/functions/src/BookSearch.js
+++ booksearch/functions/src/BookSearch.js
@@ -6,11 +6,13 @@ const functions = require('firebase-functions');
 // });
 
 //BookSearch
-if (req.query.param === undefined) {
-    // エラー
-    res.status(400).send('BookSearch param undefined');
-} else {
-    // 成功
-    console.log(req.query.param);
-    res.status(200).end(req.query.param);
-}
+module.exports = functions.https.onRequest((req, res) => {
+    if (req.query.param === undefined) {
+        // エラー
+        res.status(400).send('BookSearch param undefined');
+    } else {
+        // 成功
+        console.log(req.query.param);
+        res.status(200).end(req.query.param);
+    }
+})
