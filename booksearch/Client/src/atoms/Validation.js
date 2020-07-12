a05ee2516e5ace92d61be21559c80269217618c6
mk_Kataoka <hebishima.team@gmail.com>
2020-07-12 12:43:18 +0900

    ログイン画面　バリデーション機能

---
+++ booksearch/Client/src/atoms/Validation.js
@@ -0,0 +1,26 @@
+const mailValidation = (mail) => {
+	if (!mail) return `mailを入力してください`
+	const regex = /^[a-zA-Z0-9.!#$%&'*=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+	if (!regex.test(mail)) return '正しい形式でメールアドレスを入力してください'
+
+	return ''
+}
+
+const passwordValidation = (password) => {
+	if (!password) return 'passwordを入力してください'
+	if (password.length < 8) return 'passwordは8文字以上で入力してください'
+
+	return ''
+}
+
+class Validation {
+	static formValidate = (type, value) => {
+		switch (type) {
+			case 'mail':
+				return mailValidation(value)
+			case 'password':
+				return passwordValidation(value)
+		}
+	}
+}
+export default Validation
--- booksearch/Client/src/components/UserManage/LoginForm.js
+++ booksearch/Client/src/components/UserManage/LoginForm.js
@@ -1,83 +1,108 @@
 import React, { Component } from 'react'
-import Button from '../../atoms/Button';
-import TextBox from '../../atoms/TextBox';
-import Grid from '@material-ui/core/Grid';
-import axios from 'axios';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '../../atoms/Button'
+import TextBox from '../../atoms/TextBox'
+import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Validation from '../../atoms/Validation.js'
 
 class LoginForm extends Component {
-    constructor(props, context) {
-        super()
-        this.state = {
-            mail: '',
-            password: '',
-            errors:{},
-            loading: false
-        }
+  constructor(props, context) {
+    super()
+    this.state = {
+      info: {
+        mail: '',
+        password: ''
+      },
+      message: {
+        mail: '',
+        password: ''
+      },
+      errors: {},
+      loading: false
     }
+  }
 
-    onLogin = (event) => {
-        event.preventDefault();
-            this.setState({loading:true})
-        axios.post('/login', this.state)
-            .then((res) => {
-                this.setState({loading:false})
-                //APIにthis.stateのmailとパスワード
-                this.props.history.push('/BookSearch')
-            },
-            ).catch((error) => {
-                console.error(error);
-                this.setState({
-                    errors:error.response.data,
-                    loading:false,
-                })
-                alert('ログインエラーです');
-            }
-            );
-    }
+  handleChange = (event) => {
+    const key = event.target.name
+    const value = event.target.value
+    const { info, message } = this.state
+    this.setState({
+      info: {
+        ...info,
+        [key]: value
+      },
+      message: {
+        ...message,
+        [key]: Validation.formValidate(key, value)
+      }
+    })
+  }
 
-    handleChange = (event) => {
+  onLogin = (event) => {
+    event.preventDefault()
+    this.setState({ loading: true })
+    axios.post('/login', this.state.info)
+      .then((res) => {
+        this.setState({ loading: false })
+        //APIにthis.stateのinfo
+        this.props.history.push('/BookSearch')
+      },
+      ).catch((error) => {
+        console.error(error)
         this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-    render() {
-        const {loading}=this.state;
-        return (
-            <Grid container >
-                <Grid item xs />
-                <Grid item xs>
-                    <form onSubmit={this.onLogin}
-                        autoComplete="off">
-                        <Grid item xs>
-                            <TextBox label="mail"
-                                name="mail"
-                                value={this.state.mail}
-                                onChange={this.handleChange}>
-                            </TextBox>
-                        </Grid>
-                        <Grid item xs>
-                            <TextBox label="password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                type='password'>
-                            </TextBox>
-                        </Grid>
-                        <Grid item xs>
-                            <Button className="btnLogin"
-                                type="submit"
-                                variant="contained">
-                                ログイン
-                                {loading&&(<CircularProgress />)}
-                            </Button>
-                        </Grid>
-                    </form>
-                </Grid>
-                <Grid item sm />
-            </Grid >
-        )
-    }
+          errors: error.response.data,
+          loading: false
+        })
+        alert('ログインエラーです')
+      }
+      )
+  }
+
+  render() {
+    const { info, loading, message } = this.state
+    return (
+      <Grid container >
+        <Grid item xs />
+        <Grid item xs>
+          <form onSubmit={this.onLogin}
+            autoComplete="off">
+            <Grid item xs>
+              <TextBox label="mail"
+                name="mail"
+                value={info.mail}
+                onChange={this.handleChange}>
+              </TextBox>
+              {message.mail && (
+              <p style={{ color: 'red', fontSize: 8 }}>{message.mail}</p>
+            )}
+            </Grid>
+            <Grid item xs>
+              <TextBox label="password"
+                name="password"
+                value={info.password}
+                onChange={this.handleChange}
+                type='password'>
+              </TextBox>
+              {message.password && (
+              <p style={{ color: 'red', fontSize: 8 }}>{message.password}</p>
+            )}
+            </Grid>
+            <Grid item xs>
+              <Button className="btnLogin"
+                type="submit"
+                variant="contained"
+              >
+                ログイン
+              {loading && (<CircularProgress />)}
+              </Button>
+            </Grid>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid >
+    )
+  }
 }
 
-export default LoginForm;
+export default LoginForm
