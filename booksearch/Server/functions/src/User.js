f1a76b37778fde999eafb077477529c2bfbba8cc
hebishima shogo <hebishima.team@gmail.com>
2020-07-19 15:09:45 +0900

    ログインの戻り値にIsAdminを追加し、管理者の場合trueを設定するように修正

--- booksearch/Server/functions/src/User.js
+++ booksearch/Server/functions/src/User.js
@@ -2,26 +2,49 @@ const firebase = require('firebase');
 const config = require('../src/config');
 firebase.initializeApp(config);
 
+const firebaseDb = firebase.firestore();
+const collection = firebaseDb.collection('AdminUsers')
+
 //Login
 exports.login = (req, res) => {
   // ユーザ情報を保持
   const user = {
     mail: req.body.mail,
-    password: req.body.password
+    password: req.body.password,
+    uid: ""
   }
-
-  // res.header('Access-Control-Allow-Origin', "*");
-  // res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
 
   // firebaseによるログイン認証処理
   firebase.auth().signInWithEmailAndPassword(user.mail, user.password)
     .then(data => {
+      // ユーザIDを保持
+      user.uid = firebase.auth().currentUser.uid;
+
+      console.log("user.uid:", user.uid);
+
       // 認証に成功したらユーザのトークン情報を取得する
       return data.user.getIdToken();
     })
     .then(token => {
-      // トークンを返す
-      return res.json({ token });
+
+      // 戻り値
+      const result = { token: token, isAdmin: false };
+
+      // 管理者判定
+      var docRef = collection.doc(user.uid);
+      docRef.get().then(function (doc) {
+        if (doc.exists) {
+          console.log("管理者情報:", doc.data());
+          result.isAdmin = true;
+        }
+
+        // 戻り値を返す
+        return res.json({ result });
+
+      }).catch(function (error) {
+        console.error("ログインに失敗しました。内部エラー:", error.message);
+        return res.status(403).json({ error: "ログインに失敗しました。内部エラー:" + error.message });
+      });
     })
     .catch(error => {
       console.error(error.message);
